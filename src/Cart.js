db8b75628a0dbeb62ebab99ab8d536d1b740fc70
Tushar Sagar <sagartushar104@gmail.com>
2021-07-05 11:22:52 +0530

    Added delete functionality

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,76 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+         products :[
+                {
+                    price : 94599,
+                    qty : 1,
+                    title: "Laptop",
+                    img : '',
+                    id:1
+                },
+                {
+                    price : 999,
+                    qty : 1,
+                    title: "Mobile",
+                    img : '',
+                    id:2
+                },
+                {
+                    price : 999,
+                    qty : 1,
+                    title: "bile",
+                    img : '',
+                    id: 3
+                },
+            ]
+
+        }
+        // this.increaseQuantity(1);
+    }
+    increaseQuantity=(product)=>{
+        product.qty++;
+        this.setState({
+            product
+        })
+        
+    } 
+    decreaseQuantity=(product)=>{
+        if(product.qty > 0)
+        product.qty--;
+        this.setState({
+            product
+        })
+        
+    }
+    deleteProduct=(product)=>{
+        console.log(product);
+        const {products} = this.state;
+        const index = products.indexOf(product);
+        products.splice(index,1);
+        this.setState({
+            product
+        })
+    }   
     render(){
+        const {products} = this.state;
+        
+     
         return (
+            
             <div>
-                <CartItem />
-                <CartItem />
-                <CartItem />
+               {products.map((product)=>{
+                   return <CartItem product = {product} 
+                            key ={product.id} 
+                            increaseQuantity = {this.increaseQuantity}
+                            decreaseQuantity = {this.decreaseQuantity}
+                            deleteProduct = {this.deleteProduct}
+                        />;
+                })}
+                
             </div>
         )
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,41 +1,16 @@
 import React from 'react';
 class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            price : 999,
-            qty : 1,
-            title: "Mobile",
-            img : ''
-
-        }
-    }
-    increaseQuantity=()=>{ 
-        console.log('this',this.state);
-        
-        this.setState({   // object type
-            
-            qty : this.state.qty + 1,
-        });
-    }
-    decreaseQuantity=()=>{ 
-        console.log('this',this.state);
-        if(this.state.qty > 0){
-            this.setState({   // object type
-                qty : this.state.qty - 1,
-            });
-        }
-    }
+ 
+    
 
     render(){
-        const {price , qty , title} = this.state;
-        
+        const {price , qty , title} = this.props.product;
+        console.log(this.props);
         return(
           
             <div className = 'cart-item'>
                 <div className="left-block">
-                    {/* // eslint-disable-next-line */}
-                    <img style= {styles.image}></img>
+                  <img style= {styles.image}></img>
                    
                 </div>
                 <div className="right-block">
@@ -45,15 +20,15 @@ class CartItem extends React.Component{
                     <div className="cart-item-actions">
                         <img alt ="increase" className="action-icons" 
                             src="https://image.flaticon.com/icons/png/512/992/992651.png" 
-                            onClick = {this.increaseQuantity}></img>
+                            onClick = {()=>this.props.increaseQuantity(this.props.product)}></img>
                         <img alt ="decrease" className="action-icons" 
                             src="https://image.flaticon.com/icons/png/512/992/992683.png" 
-                            onClick = {this.decreaseQuantity}
+                            onClick = {()=>this.props.decreaseQuantity(this.props.product)}
                             ></img>
                         <img alt ="delete" 
                             className="action-icons" 
                             src="https://image.flaticon.com/icons/png/512/1214/1214428.png"
-                            onClick = {this.increaseQuantity}
+                            onClick = {()=>this.props.deleteProduct(this.props.product)}
                            ></img>
                     </div>
                 </div>
